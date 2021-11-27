26fd50dbe2f9f2139eb36224412cb4819eb7062a
Ryan Williams <codereese@gamil.com>
2021-11-27 12:21:00 -0600

    testing clrf

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
    <Layout pageTitle="Home Page">
-     <p>I'm making this by following the Gatsby Tutorial.</p>
+     <p>I'm making this change to test the CLRF by following the Gatsby Tutorial.</p>
      <StaticImage
       alt="Carlin face"
       src="../images/halloweenCHangemymind.png"
