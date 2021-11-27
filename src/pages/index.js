ec7d7c273e837f11162423278431238a39899701
Ryan Williams <codereese@gamil.com>
2021-11-27 12:19:07 -0600

    finished tutorial part 3 with plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 // Step 2: Define your component
 const IndexPage = () => {
   return (
    <Layout pageTitle="Home Page">
      <p>I'm making this by following the Gatsby Tutorial.</p>
+     <StaticImage
+      alt="Carlin face"
+      src="../images/halloweenCHangemymind.png"
+     />
    </Layout>
   )
 }
-
+//"https://api.time.com/wp-content/uploads/2017/09/carlin.jpeg?w=800&quality=85"
 // Step 3: Export your component
 export default IndexPage
